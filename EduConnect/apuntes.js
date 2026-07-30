56af28fed048dad3780b7552c486d7b30704a4de
Arcxcad <deadman123543@gmail.com>
2026-07-30 12:24:13 -0300

    Add files via upload

--- EduConnect/apuntes.js
+++ EduConnect/apuntes.js
@@ -1,4 +1,4 @@
-// Oculta los aputes privados si el usuario no está logueado
+// Oculta los apuntes privados si el usuario no está logueado
 /*const usuarioLogueado = false;
 
 document.querySelectorAll(".privado").forEach(div => {
@@ -7,18 +7,19 @@ document.querySelectorAll(".privado").forEach(div => {
   }
 });*/
 
+// Función para crear un apunte
 function crearApunte(texto, contenedorId) {
   const contenedor = document.getElementById(contenedorId);
 
-  // Crear div para el apunte
+  // Crea el div para el apunte
   const div = document.createElement("div");
   div.className = "item";
 
-  // Crear párrafo con el texto
+  // Crea el parrafo con el texto apunte privado o publico
   const p = document.createElement("p");
   p.textContent = texto;
 
-  // Botón editar
+  // Boton editar
   const editarBtn = document.createElement("button");
   editarBtn.textContent = "Editar";
   editarBtn.onclick = () => {
@@ -28,17 +29,17 @@ function crearApunte(texto, contenedorId) {
     }
   };
 
-  // Botón borrar
+  // Boton borrar
   const borrarBtn = document.createElement("button");
   borrarBtn.textContent = "Borrar";
   borrarBtn.onclick = () => div.remove();
 
-  // Agregar todo al div
+  // Agrega los apuntes en todo el div correspondiente
   div.appendChild(p);
   div.appendChild(editarBtn);
   div.appendChild(borrarBtn);
 
-  // Insertar en el contenedor correcto
+  // Insertar el apunte en el contenedor correcto
   contenedor.appendChild(div);
 }
 
